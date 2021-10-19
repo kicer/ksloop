1953b2315547a80a3f9fc7991f4ae3515efcba5b
vc4 <287920192@qq.com>
2021-10-19 20:53:43 +0800

    clear misc

--- src/main.c
+++ src/main.c
@@ -4,32 +4,31 @@
 
 
 volatile uint32_t systick = 0;
+/* 系统时钟
+ * 采用内部RCH时钟作为主时钟，上电后默认为4MHz
+ * 进入DeepSleep模式后会自动关闭
+ */
 volatile uint32_t core_clock_hz = 4000000; /* 4MHz */
+/* RTC时钟
+ * 超低功耗外设使用外部XTL晶振作为其时钟
+ */
 
-#define LED_PIN   5
 
+#define BUZZER_PIN    7
 int main (void) {
     SysTick_Config( core_clock_hz / 1000 );
 
-    CLOCK_PERI_CLKEN |= BIT(CLKEN_PERI_GPIO);
-    P1_ADS &= ~BIT(LED_PIN);
-    P1_DIR &= ~BIT(LED_PIN);
-    P1_OUT &= ~BIT(LED_PIN);
-
-/* offical op code
-    M0P_CLOCK->PERI_CLKEN_f.GPIO = 1;
-    M0P_GPIO->P15_SEL = 0;
-    M0P_GPIO->P1DIR_f.P15 = 0;
-    M0P_GPIO->P1PU_f.P15 = 0;
-    M0P_GPIO->P1OUT_f.P15 = 0;
-*/
+    M0P_SYSCTRL->PERI_CLKEN_f.GPIO = 1;
+    M0P_GPIO->PA07_SEL = 0;
+    M0P_GPIO->PADIR_f.PA07 = 0;
+    M0P_GPIO->PAOUT_f.PA07 = 1;
 
     while(1) {
         uint32_t next = systick + 1000;
         while(systick<next) {
             __asm__("WFI");
         }
-        P1_OUT ^=  BIT(LED_PIN);
+        M0P_GPIO->PAOUT ^= BIT(BUZZER_PIN);
     }
 
     return 0;
@@ -37,5 +36,5 @@ int main (void) {
 
 
 void SysTick_IRQn_handler(void) {
-  ++systick;
+    ++systick;
 }
