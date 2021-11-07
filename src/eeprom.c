e1ece8340606e950e375f638358df2ac07f580fd
vc4 <287920192@qq.com>
2021-11-07 12:07:41 +0800

    bug fix of flash op

--- src/eeprom.c
+++ src/eeprom.c
@@ -10,8 +10,10 @@
 
 int eeprom_init(void) {
 #if (CORE_CLOCK_HZ == 4000000ul)
-    FLASH_UNLOCK();
-    M0P_FLASH->CR_f.WAIT = 0;
+    while(M0P_FLASH->CR_f.WAIT != 0) {
+        FLASH_UNLOCK();
+        M0P_FLASH->CR = 0x00;
+    }
 #else
     /* need setting TNVS/TPGS/TPROG... */
 #error "Not support this core.clock with eeprom"
@@ -31,27 +33,42 @@ int eeprom_read(uint32_t addr, uint8_t *data, int size) {
 }
 
 int eeprom_write(uint32_t addr, uint8_t *data, int size) {
-    FLASH_UNLOCK();
-    M0P_FLASH->SLOCK_f.SLOCK = 0xFFFF;
+    while(M0P_FLASH->SLOCK_f.SLOCK != 0xFFFF) {
+        FLASH_UNLOCK();
+        M0P_FLASH->SLOCK = 0xFFFF;
+    }
     while(M0P_FLASH->CR_f.BUSY==1){};
-    M0P_FLASH->CR_f.OP = 1; /* program */
+    while(M0P_FLASH->CR_f.OP != 1) {
+        FLASH_UNLOCK();
+        M0P_FLASH->CR = 0x01; /* program */
+    }
     for(int i=0; i<size; i++) {
         *((volatile uint8_t*)(addr+i)) = data[i];
         while(M0P_FLASH->CR_f.BUSY==1){};
     }
     FLASH_UNLOCK();
-    M0P_FLASH->SLOCK_f.SLOCK = 0x0000;
+    M0P_FLASH->SLOCK = 0x0000;
+    for(int i=0; i<size; i++) {
+        if(data[i] != *((volatile uint8_t*)(addr+i))) {
+            return -1;
+        }
+    }
     return 0;
 }
 
 int eeprom_sector_erase(uint32_t addr) {
-    FLASH_UNLOCK();
-    M0P_FLASH->SLOCK_f.SLOCK = 0xFFFF;
+    while(M0P_FLASH->SLOCK_f.SLOCK != 0xFFFF) {
+        FLASH_UNLOCK();
+        M0P_FLASH->SLOCK = 0xFFFF;
+    }
     while(M0P_FLASH->CR_f.BUSY==1){};
-    M0P_FLASH->CR_f.OP = 2; /* sector erase */
+    while(M0P_FLASH->CR_f.OP != 2) {
+        FLASH_UNLOCK();
+        M0P_FLASH->CR = 0x02; /* sector erase */
+    }
     *((volatile uint8_t*)addr) = 0x00;
     while(M0P_FLASH->CR_f.BUSY==1){};
     FLASH_UNLOCK();
-    M0P_FLASH->SLOCK_f.SLOCK = 0x0000;
+    M0P_FLASH->SLOCK = 0x0000;
     return 0;
 }
