a3cc94f6c7b482eb3278043ae2ad5fe68ae49770
vc4 <287920192@qq.com>
2022-07-02 09:15:17 +0800

    使用RCH作为系统时钟，方便快速运行降低功耗

--- src/main.c
+++ src/main.c
@@ -7,7 +7,7 @@
 static void app_clk_init(void) {
     /* 系统时钟
      * 上电后默认为内部RCH时钟4MHz，
-     * 需要切换到RCL=32.768kHz
+     * 需要同时使能RCL=32.768kHz
      */
     /* step 1: startup=256, load trim */
     M0P_CLOCK->RCL_CR = (*cast(volatile uint16_t *, 0x00100C22ul)) |
@@ -16,16 +16,10 @@ static void app_clk_init(void) {
     M0P_CLOCK->SYSCTRL2 = 0x5A5A;
     M0P_CLOCK->SYSCTRL2 = 0xA5A5;
     M0P_CLOCK->SYSCTRL0_f.RCL_EN = 1;
-    /* step 3: wait stable */
+    /* step 3: wait RCL stable */
     while(1!=M0P_CLOCK->RCL_CR_f.STABLE) {};
-    /* step 4: switch to rcl */
-    M0P_CLOCK->SYSCTRL2 = 0x5A5A;
-    M0P_CLOCK->SYSCTRL2 = 0xA5A5;
-    M0P_CLOCK->SYSCTRL0_f.CLK_SW4_SEL = 2;
-    /* step 5: close rch */
-    M0P_CLOCK->SYSCTRL2 = 0x5A5A;
-    M0P_CLOCK->SYSCTRL2 = 0xA5A5;
-    M0P_CLOCK->SYSCTRL0_f.RCH_EN = 0;
+    /* step 4: wait RCH stable */
+    while(1!=M0P_CLOCK->RCH_CR_f.STABLE) {};
 }
 
 
